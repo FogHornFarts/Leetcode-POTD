56afac8390e3ca3611f683aef084fc30e9e2acad
FogHornFarts <saimoturi402@gmail.com>
2026-02-06 22:44:48 +0530

    Time: 24 ms (94.19%), Space: 105.1 MB (52.91%) - LeetHub

---
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int left = 0;
+        int n = nums.size();
+        for(int right = 0;right<n;right++){
+            if((long long)nums[left]*k<nums[right]){
+                left++;
+            }
+        }
+        return left;
+    }
+};
