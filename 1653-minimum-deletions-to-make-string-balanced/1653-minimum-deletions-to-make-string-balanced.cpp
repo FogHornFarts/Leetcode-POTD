768060286a326c05cd1ffb93a3b1ebec15e28edd
FogHornFarts <saimoturi402@gmail.com>
2026-02-07 22:43:31 +0530

    Time: 7 ms (97.78%), Space: 25.6 MB (54.88%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int bCount = 0;
+        int b_count = 0;
         int ans = 0;
-        for(char c:s){
-            if(c=='b'){
-                bCount++;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='b'){
+                b_count++;
             }else{
-                ans = min(ans+1, bCount);
+                ans = min(ans+1, b_count);
             }
         }
         return ans;
