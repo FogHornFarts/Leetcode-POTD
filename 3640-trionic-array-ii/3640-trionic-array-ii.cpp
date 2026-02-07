c11b76740c830ec6bd10b6c9c848b77996c45491
FogHornFarts <saimoturi402@gmail.com>
2026-02-07 22:59:31 +0530

    Time: 3 ms (76.08%), Space: 132.5 MB (91%) - LeetHub

--- 3640-trionic-array-ii/3640-trionic-array-ii.cpp
+++ 3640-trionic-array-ii/3640-trionic-array-ii.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     long long maxSumTrionic(vector<int>& nums) {
-        long long ans = LLONG_MIN;
+        const long long NEG = -(long long)4e18;
 
-        int left = 0, p = 0, q = 0;
-        long long sum = nums[0];
+        long long dp0 = nums[0];   // start fresh at i
+        long long dp1 = NEG;       // in 1st increasing segment
+        long long dp2 = NEG;       // in decreasing segment
+        long long dp3 = NEG;       // in 2nd increasing segment
+        long long ans = NEG;
 
-        for (int right = 1; right < nums.size(); right++) {
-            sum += nums[right];
+        for (int i = 1; i < (int)nums.size(); i++) {
+            long long x = nums[i];
 
-            // start decreasing phase
-            if (nums[right - 1] > nums[right]) {
-                if (right >= 2 && nums[right - 2] < nums[right - 1]) {
-                    p = right - 1;
+            if (nums[i] > nums[i - 1]) {
+                // update dp3 first because it depends on old dp2
+                if (dp3 != NEG) dp3 = dp3 + x;
+                if (dp2 != NEG) dp3 = max(dp3, dp2 + x);
 
-                    // shrink left greedily
-                    while (left < q || (nums[left] < 0 && left + 1 < p)) {
-                        sum -= nums[left++];
-                    }
-                }
+                // dp1 can extend or start from dp0
+                if (dp1 != NEG) dp1 = dp1 + x;
+                dp1 = max(dp1, dp0 + x);
+
+                dp2 = NEG;
             }
-            // start final increasing phase
-            else if (nums[right - 1] < nums[right]) {
-                if (right >= 2 && nums[right - 2] > nums[right - 1]) {
-                    q = right - 1;
-                }
+            else if (nums[i] < nums[i - 1]) {
+                // dp2 can extend or start from dp1
+                if (dp2 != NEG) dp2 = dp2 + x;
+                if (dp1 != NEG) dp2 = max(dp2, dp1 + x);
 
-                // valid trionic window exists
-                if (left != p) {
-                    ans = max(ans, sum);
-                }
+                dp1 = dp3 = NEG;
             }
-            // equal → reset
             else {
-                left = p = q = right;
-                sum = nums[right];
+                dp1 = dp2 = dp3 = NEG;
             }
+
+            dp0 = x;
+            ans = max(ans, dp3);
         }
 
         return ans;
