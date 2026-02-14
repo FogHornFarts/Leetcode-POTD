f712508b591f85883339ef49cf0943da752d7262
FogHornFarts <saimoturi402@gmail.com>
2026-02-14 23:50:32 +0530

    Time: 7 ms (44.31%), Space: 18.8 MB (31.48%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<vector<double>> dp(101, vector<double>(101, 0.0));
-        dp[0][0] = poured;
+        vector<double> dp(102, 0.0);
+        dp[0] = poured;
 
         for(int r = 0; r < query_row; r++) {
+            vector<double> next(102, 0.0);
+
             for(int c = 0; c <= r; c++) {
-                double overflow = max(0.0, dp[r][c] - 1.0);
+                double overflow = max(0.0, dp[c] - 1.0);
                 if(overflow > 0) {
-                    dp[r+1][c] += overflow / 2.0;
-                    dp[r+1][c+1] += overflow / 2.0;
+                    next[c]     += overflow / 2.0;
+                    next[c + 1] += overflow / 2.0;
                 }
             }
+
+            dp = next;
         }
 
-        return min(1.0, dp[query_row][query_glass]);
+        return min(1.0, dp[query_glass]);
     }
 };
