514635cd3cbe110478526d5f32bbb265c9704095
FogHornFarts <saimoturi402@gmail.com>
2026-02-16 10:49:46 +0530

    Time: 5 ms (5.56%), Space: 8.6 MB (6.51%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) {
-
-        vector<int> bits;
-
-        // Step 1: Extract all 32 bits (including leading zeros)
-        for(int i = 0; i < 32; i++){
-            bits.push_back(n % 2);
-            n /= 2;
+    int convert_back(vector<int>bits){
+        int num=bits[31];
+        for(int i=0;i<31;i++){
+            num=num+bits[i]*pow(2,31-i);
         }
-
-        // Step 2: bits currently = reversed bits already (LSB → MSB)
-        // So we can directly convert it to decimal.
-
-        uint32_t ans = 0;
-        uint32_t base = 1;
-
-        for(int i = 31; i >= 0; i--){
-            if(bits[i] == 1){
-                ans += base;
-            }
-            base *= 2;
+        return num;
+    }
+    int reverseBits(int n) {
+        vector<int>bits(32,0);
+        int num=n,i=0;
+        while(num!=0){
+            int rem=num%2;
+            bits[i]=rem;
+            num=num/2;
+            i++;
         }
-
-        return ans;
+        return convert_back(bits);
     }
 };
