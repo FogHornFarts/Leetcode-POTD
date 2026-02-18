4c94b425f35f602e7a8c20baccb837932570e89d
FogHornFarts <saimoturi402@gmail.com>
2026-02-18 09:59:11 +0530

    Time: 0 ms (100%), Space: 7.6 MB (91.43%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        string binary_str = bitset<32>(n).to_string();
-        size_t first_one = binary_str.find_first_of('1');
-        if (first_one == std::string::npos) {
-            binary_str = "0"; 
-        } else {
-            binary_str = binary_str.substr(first_one);
-        }
-        for (size_t i = 0; i<binary_str.length()-1;++i) {
-            if (binary_str[i] == binary_str[i + 1]) {
-                return false; 
-            }
-        }
-        return true;
+        unsigned int x = n ^ (n >> 1);
+        return (x & (x + 1)) == 0;
     }
 };
