96f90750de228171e17241ce62e4f3b0eb526c4e
FogHornFarts <saimoturi402@gmail.com>
2026-02-19 12:59:03 +0530

    Time: 4 ms (52.74%), Space: 13.1 MB (32.17%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.cpp
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -1,21 +1,20 @@
-class Solution {
-public:
-    int countBinarySubstrings(string s) {
-        vector<int> valid;
-        int cnt = 1;
-        for(int i=1;i<(int)s.size();i++){
-            if(s[i]==s[i-1]){
-                cnt++;
+class Solution{
+    public: 
+    int countBinarySubstrings(string s){
+        int prev = 0;
+        int curr = 1;
+        int ans = 0;
+        
+        for(int i=1;i<s.size();i++){
+            if(s[i] == s[i-1]){
+                curr++;
             }else{
-                valid.push_back(cnt);
-                cnt = 1;
+                ans+=min(prev, curr);
+                prev = curr;
+                curr = 1;
             }
         }
-        valid.push_back(cnt);
-        int ans = 0;
-        for(int i=1;i<(int)valid.size();i++){
-            ans+=min(valid[i-1],valid[i]);
-        }
+        ans+= min(prev, curr);
         return ans;
     }
 };
