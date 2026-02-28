bc94eabf52fe9772baf22047bd7da300071f940a
FogHornFarts <saimoturi402@gmail.com>
2026-02-28 15:38:39 +0530

    Time: 20 ms (97.99%), Space: 8.1 MB (41.61%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -3,7 +3,7 @@ public:
     int concatenatedBinary(int n) {
         const int MOD = 1e9 + 7;
         long long ans = 0;
-        int len = 0;
+        int len = 0; 
         for (int i = 1; i <= n; ++i) {
             if ((i & (i - 1)) == 0) {
                 ++len;
