b3cc1298b68996e283990cec1815e51656e27343
FogHornFarts <saimoturi402@gmail.com>
2026-03-02 22:25:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
+++ 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -1,34 +1,34 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     int minSwaps(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<int> trailing(n, 0);
+        vector<int> zeros(n, 0);
         for (int i = 0; i < n; ++i) {
             int cnt = 0;
             for (int j = n - 1; j >= 0; --j) {
-                if (grid[i][j] == 0)
-                    cnt++;
-                else
-                    break;
+                if (grid[i][j] == 0) ++cnt;
+                else break;
             }
-            trailing[i] = cnt;
+            zeros[i] = cnt;
         }
         vector<int> order(n);
-        iota(order.begin(), order.end(), 0);
+        for (int i = 0; i < n; ++i) order[i] = i;
+
         int ans = 0;
         for (int i = 0; i < n; ++i) {
             int need = n - 1 - i;
             int j = i;
-            while (j < n && trailing[order[j]] < need)
-                ++j;
-            if (j == n)
-                return -1;
+            while (j < n && zeros[order[j]] < need) ++j;
+            if (j == n) return -1; 
             ans += j - i;
-            int temp = order[j];
+            int tmp = order[j];
             for (int k = j; k > i; --k) {
                 order[k] = order[k - 1];
             }
-            order[i] = temp;
+            order[i] = tmp;
         }
         return ans;
     }
