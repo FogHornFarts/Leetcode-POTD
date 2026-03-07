34bc7b06c774bad2e1370fa726d84c46df450053
FogHornFarts <saimoturi402@gmail.com>
2026-03-07 17:50:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -2,22 +2,41 @@ class Solution {
 public:
     int minFlips(string s) {
         int n = s.length();
-        string doubled = s+s;
+        string doubled = s + s;
         string pattern1 = "";
         string pattern2 = "";
-        for(int i=0;i<2*n;i++){
-            pattern1+=(i%2==0) ? '0' : '1';
-            pattern2+=(i%2==0) ? '1' : '0';
+        for(int i = 0; i < 2 * n; i++){
+            if(i % 2 == 0){
+                pattern1 += '0';
+            } 
+            else{
+                pattern1 += '1';
+            }
+            if(i % 2 == 0){
+                pattern2 += '1';
+            } 
+            else{
+                pattern2 += '0';
+            }
         }
-        vector<int> diff1(2*n+1,0);
-        vector<int> diff2(2*n+1,0);
-
-        for(int i=0;i<2*n;i++){
-            diff1[i+1] = diff1[i]+(doubled[i]!=pattern1[i] ? 1 : 0);
-            diff2[i+1] = diff2[i]+(doubled[i]!=pattern2[i] ? 1 : 0);
+        vector<int> diff1(2*n + 1, 0);
+        vector<int> diff2(2*n + 1, 0);
+        for(int i = 0; i < 2 * n; i++){
+            if(doubled[i] != pattern1[i]){
+                diff1[i+1] = diff1[i] + 1;
+            }
+            else{
+                diff1[i+1] = diff1[i];
+            }
+            if(doubled[i] != pattern2[i]){
+                diff2[i+1] = diff2[i] + 1;
+            }
+            else{
+                diff2[i+1] = diff2[i];
+            }
         }
-        int  minFlips = n;
-        for(int i=0;i<n;i++){
+        int minFlips = n;
+        for(int i = 0; i < n; i++){
             int flips1 = diff1[i+n] - diff1[i];
             int flips2 = diff2[i+n] - diff2[i];
             minFlips = min(minFlips, min(flips1, flips2));
