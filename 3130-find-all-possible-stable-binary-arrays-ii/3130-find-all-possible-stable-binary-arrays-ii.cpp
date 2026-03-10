24ae1b5a366a9472a96cadd84f7f99b9135b25cf
FogHornFarts <saimoturi402@gmail.com>
2026-03-10 22:12:50 +0530

    Time: 135 ms (72.6%), Space: 159.5 MB (52.05%) - LeetHub

--- 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
+++ 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int numberOfStableArrays(int zero, int one, int limit) {
-        const int mod = 1e9 + 7;
-        int li = limit + 1;
-        vector<vector<long long>> dp0(zero + 1, vector<long long>(one + 1, 0));
-        vector<vector<long long>> dp1(zero + 1, vector<long long>(one + 1, 0));
-        for (int i = 1; i <= min(zero, limit); i++) {
-            dp0[i][0] = 1;
+        const int MOD = 1e9 + 7;
+        vector<vector<array<long,2>>> dp(
+            zero+1, vector<array<long,2>>(one+1, {0LL,0LL})
+        );
+        for(int i=1;i<=min(zero,limit);i++){
+            dp[i][0][0] = 1;
         }
-        for (int j = 1; j <= min(one, limit); j++) {
-            dp1[0][j] = 1;
+        for(int j=1;j<=min(one,limit);j++){
+            dp[0][j][1] = 1;
         }
-        for (int i = 1; i <= zero; i++) {
-            for (int j = 1; j <= one; j++) {
-                long long sub1 = (i >= li ? dp1[i - li][j] : 0);
-                dp0[i][j] = (dp0[i - 1][j] + dp1[i - 1][j] - sub1 + mod) % mod;
-                long long sub2 = (j >= li ? dp0[i][j - li] : 0);
-                dp1[i][j] = (dp0[i][j - 1] + dp1[i][j - 1] - sub2 + mod) % mod;
+        for(int i=1;i<=zero;i++){
+            for(int j=1;j<=one;j++){
+                long over0 = (i-limit >= 1) ? dp[i-limit-1][j][1] : 0;
+                long over1 = (j-limit >= 1) ? dp[i][j-limit-1][0] : 0;
+                dp[i][j][0] = (dp[i-1][j][0] + dp[i-1][j][1] - over0 + MOD) % MOD;
+                dp[i][j][1] = (dp[i][j-1][0] + dp[i][j-1][1] - over1 + MOD) % MOD;
             }
         }
-        return (dp0[zero][one] + dp1[zero][one]) % mod;
+        return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
     }
 };
