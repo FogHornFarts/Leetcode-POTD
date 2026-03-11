ef61cd2f4beb2ffa3431498024f5dbc8149ae32e
FogHornFarts <saimoturi402@gmail.com>
2026-03-11 18:46:32 +0530

    Time: 0 ms (100%), Space: 8 MB (4.02%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
-
-        if(n == 0){
+        if(n==0){
             return 1;
         }
         string bin_rev = "";
-        while(n > 0){
-            if(n % 2 == 0){
+        while(n>0){
+            if(n%2==0){
                 bin_rev += '1';
-            } else {
+            }else{
                 bin_rev += '0';
             }
-            n /= 2;
+            n/=2;
         }
-        reverse(bin_rev.begin(), bin_rev.end());
+        reverse(bin_rev.begin(),bin_rev.end());
         int res = 0;
-        for(char c : bin_rev){
-            res = res * 2 + (c - '0');
+        for(char c: bin_rev){
+            res = res*2+(c-'0');
         }
         return res;
     }
