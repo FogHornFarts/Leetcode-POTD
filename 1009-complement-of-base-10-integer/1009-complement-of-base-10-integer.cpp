e1b4bacc0e6c6dffdebf8e233b4a59ef41fff25e
FogHornFarts <saimoturi402@gmail.com>
2026-03-11 14:47:42 +0530

    Time: 0 ms (100%), Space: 7.8 MB (59.24%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+
+        if(n == 0) return 1;
+
+        string bin_rev = "";
+
+        while(n > 0){
+            if(n % 2 == 0){
+                bin_rev += '1';
+            } else {
+                bin_rev += '0';
+            }
+
+            n /= 2;
+        }
+
+        reverse(bin_rev.begin(), bin_rev.end());
+
+        int res = 0;
+
+        for(char c : bin_rev){
+            res = res * 2 + (c - '0');
+        }
+
+        return res;
+    }
+};
