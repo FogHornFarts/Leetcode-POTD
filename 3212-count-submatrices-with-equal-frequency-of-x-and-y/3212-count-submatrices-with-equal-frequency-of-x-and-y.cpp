627d8cb51e24071f15f32f8ef309dc1a42a9b4c0
FogHornFarts <saimoturi402@gmail.com>
2026-03-19 20:34:09 +0530

    Time: 21 ms (95.93%), Space: 101.1 MB (92.68%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -5,23 +5,21 @@ public:
         vector<int> sumX(cols, 0);
         vector<int> sumY(cols, 0);
         int res = 0;
-
-        for (int i = 0; i < rows; i++) {
+        for(int i=0;i<rows;i++){
             int rx = 0, ry = 0;
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == 'X') {
+            for(int j=0;j<cols;j++){
+                if(grid[i][j] == 'X'){
                     rx++;
                 }else if(grid[i][j] == 'Y'){
                     ry++;
                 }
                 sumX[j] += rx;
                 sumY[j] += ry;
-                if (sumX[j] > 0 && sumX[j] == sumY[j]) {
+                if(sumX[j] > 0 && sumX[j] == sumY[j]){
                     res++;
                 }
             }
         }
-
         return res;
     }
 };
