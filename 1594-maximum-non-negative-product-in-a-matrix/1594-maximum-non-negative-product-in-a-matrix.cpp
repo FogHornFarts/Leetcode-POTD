bcb411091cbaadf17dfd502932632b3ec2bef3a8
FogHornFarts <saimoturi402@gmail.com>
2026-03-24 01:11:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
-    int maxProductPath(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
+    int maxProductPath(vector<vector<int>>& b) {
+        int n = b.size();
+        int m = b[0].size();
         const long long MOD = 1e9 + 7;
 
-        vector<vector<long long>> maxDp(n, vector<long long>(m));
-        vector<vector<long long>> minDp(n, vector<long long>(m));
+        vector<vector<long long>> dpMax(n, vector<long long>(m));
+        vector<vector<long long>> dpMin(n, vector<long long>(m));
 
-        maxDp[0][0] = minDp[0][0] = grid[0][0];
-        for(int j=1;j<m;j++){
-            maxDp[0][j] = minDp[0][j] = maxDp[0][j-1] * grid[0][j];
-        }
-        for(int i=1;i<n;i++){
-            maxDp[i][0] = minDp[i][0] = maxDp[i-1][0] * grid[i][0];
-        }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                long long val = grid[i][j];
-                long long a = maxDp[i -1][j] * val;
-                long long b = minDp[i -1][j] * val;
-                long long c = maxDp[i][j-1] * val;
-                long long d = minDp[i][j-1] * val;
-                maxDp[i][j] = max({a, b, c, d});
-                minDp[i][j] = min({a, b, c, d});
+        dpMax[0][0] = dpMin[0][0] = b[0][0];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0){
+                    continue;
+                }
+                long long val = b[i][j];
+                long long mx = LLONG_MIN;
+                long long mn = LLONG_MAX;
+
+                if (i > 0) {
+                    mx = max(mx, max(dpMax[i - 1][j] * val, dpMin[i - 1][j] * val));
+                    mn = min(mn, min(dpMax[i - 1][j] * val, dpMin[i - 1][j] * val));
+                }
+
+                if (j > 0) {
+                    mx = max(mx, max(dpMax[i][j - 1] * val, dpMin[i][j - 1] * val));
+                    mn = min(mn, min(dpMax[i][j - 1] * val, dpMin[i][j - 1] * val));
+                }
+
+                dpMax[i][j] = mx;
+                dpMin[i][j] = mn;
             }
         }
-        long long ans = maxDp[n-1][m-1];
-        if(ans < 0){
+        if (dpMax[n - 1][m - 1] < 0){
             return -1;
         }
-        return ans % MOD;
+        return dpMax[n - 1][m - 1] % MOD;
     }
 };
