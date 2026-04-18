be01fc82d11d4911287e2252609fbf6bb530fe46
FogHornFarts <saimoturi402@gmail.com>
2026-04-18 13:50:28 +0530

    Time: 0 ms (100%), Space: 8.6 MB (60.81%) - LeetHub

--- 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
@@ -3,13 +3,13 @@ public:
     int mirrorDistance(int n) {
         return abs(n-reverse(n));
     }
-    int reverse(int n){
+    int reverse(int num){
         int reversed_num = 0;
-        int digits;
-        while(n>0){
-            digits = n%10;
-            reversed_num = (reversed_num*10) + digits;
-            n/=10;
+        int remainder;
+        while(num!=0){
+            remainder = num%10;
+            reversed_num = reversed_num *10+remainder;
+            num/=10;
         }
         return reversed_num;
     }
