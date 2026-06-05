76174300bfad62edf8c637419bcc12cd949f4d43
FogHornFarts <saimoturi402@gmail.com>
2026-06-05 23:10:56 +0530

    Time: 7 ms (97.46%), Space: 9 MB (100%) - LeetHub

--- 3753-total-waviness-of-numbers-in-range-ii/3753-total-waviness-of-numbers-in-range-ii.cpp
+++ 3753-total-waviness-of-numbers-in-range-ii/3753-total-waviness-of-numbers-in-range-ii.cpp
@@ -1,80 +1,48 @@
+using ll = long long;
 class Solution {
+    static inline int waves[570];
+    static inline bool init = []() {
+        int j = 0;
+        for (int i = 0; i < 1000; i++) {
+            int r = i % 10;
+            int m = (i / 10) % 10;
+            int l = (i / 100) % 10;
+            if ((m > max(l, r)) | (m < min(l, r)))
+                waves[j++] = i;
+        }
+        return 0;
+    }();
+
 public:
-    struct Node {
-        long long cnt;
-        long long wav;
-    };
+    ll totalWaviness(ll A, ll B) { return waveCount(B) - waveCount(A - 1); }
 
-    string s;
-    Node dp[20][2][3][11][11];
-    bool vis[20][2][3][11][11];
-
-    static int isWave(int left, int mid, int right) {
-        return ((mid > left && mid > right) ||
-                (mid < left && mid < right)) ? 1 : 0;
+private:
+    ll waveCount(ll num) {
+        if (num < 100)
+            return 0;
+        return accumulate(begin(waves), end(waves), 0LL,
+                          [&](ll a, int p) { return a + countWays(num, p); });
     }
 
-    Node dfs(int pos, bool tight, bool started,
-             int lenState, int last1, int last2) {
+    ll countWays(ll num, int pattern) {
+        ll t = pattern < 100;
+        ll count = 0, mult = 1;
 
-        if (pos == (int)s.size()) {
-            return {1, 0};
+        for (int i = 0; mult * 100 <= num; i++) {
+            ll pre = num / (mult * 1000);
+            ll cur = (num / mult) % 1000;
+            ll suf = num % mult;
+            ll ways = 0;
+            if (cur > pattern) {
+                ways = pre - t + 1;
+            } else if (cur == pattern) {
+                ways = max(0LL, pre - t);
+                count += suf + 1;
+            } else
+                ways = max(0LL, pre - t);
+            count += ways * mult;
+            mult *= 10;
         }
-
-        if (!tight && vis[pos][started][lenState][last1][last2]) {
-            return dp[pos][started][lenState][last1][last2];
-        }
-
-        int limit = tight ? (s[pos] - '0') : 9;
-
-        long long totalCnt = 0;
-        long long totalWav = 0;
-
-        for (int d = 0; d <= limit; d++) {
-            bool ntight = tight && (d == limit);
-
-            if (!started && d == 0) {
-                Node nxt = dfs(pos + 1, ntight, false, 0, 10, 10);
-
-                totalCnt += nxt.cnt;
-                totalWav += nxt.wav;
-            } else {
-                if (!started) {
-                    Node nxt = dfs(pos + 1, ntight, true, 1, d, 10);
-
-                    totalCnt += nxt.cnt;
-                    totalWav += nxt.wav;
-                }
-                else if (lenState == 1) {
-                    Node nxt = dfs(pos + 1, ntight, true, 2, d, last1);
-
-                    totalCnt += nxt.cnt;
-                    totalWav += nxt.wav;
-                }
-                else { 
-                    int add = isWave(last2, last1, d);
-
-                    Node nxt = dfs(pos + 1, ntight, true, 2, d, last1);
-
-                    totalCnt += nxt.cnt;
-                    totalWav += nxt.wav + nxt.cnt * 1LL * add;
-                }
-            }
-        }
-        Node res = {totalCnt, totalWav};
-        if (!tight) {
-            vis[pos][started][lenState][last1][last2] = true;
-            dp[pos][started][lenState][last1][last2] = res;
-        }
-        return res;
-    }
-    long long solve(long long N) {
-        if (N < 0) return 0;
-        s = to_string(N);
-        memset(vis, 0, sizeof(vis));
-        return dfs(0, true, false, 0, 10, 10).wav;
-    }
-    long long totalWaviness(long long num1, long long num2) {
-        return solve(num2) - solve(num1 - 1);
+        return count;
     }
 };
