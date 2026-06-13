f7ab6dff0daa63c6fe3ca07ffcf47f677648f28a
FogHornFarts <saimoturi402@gmail.com>
2026-06-13 18:45:54 +0530

    Time: 0 ms (100%), Space: 43.4 MB (95.02%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -3,11 +3,9 @@ public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
         string result;
         for(int i=0;i<words.size();i++){
-            string curr = words[i];
             int sum = 0;
-            for(char c: curr){
-                int index = c -'a';
-                sum+=weights[index];
+            for(char c: words[i]){
+                sum+=weights[c-'a'];
             }
             int res_num = sum % 26;
             result += 'z' - res_num;
