cb74e4a9860577653aa0e734f02c72580c160a0f
FogHornFarts <saimoturi402@gmail.com>
2026-06-23 00:57:25 +0530

    Time: 0 ms (100%), Space: 8.8 MB (84.03%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int b = 0, a = 0, l = 0, o = 0, n = 0; 
+        for (char ch : text){
+            if (ch == 'b'){
+                b++;
+            }
+            else if (ch == 'a'){
+                a++;
+            }
+            else if (ch == 'l'){
+                l++;
+            }
+            else if (ch == 'o'){
+                o++;
+            }
+            else if (ch == 'n'){
+                n++;
+            }
+        }
+        l = l / 2;
+        o = o / 2;
+        int ans = min(l, o);
+        ans = min(ans, b);
+        ans = min(ans, a);
+        ans = min(ans, n);
+        return ans;
+    }
+};
